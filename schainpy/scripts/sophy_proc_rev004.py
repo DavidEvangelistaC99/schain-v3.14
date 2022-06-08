291376e0da40ccf57710d87a43dbcfb08da3a71c
avaldezp <avaldezp@uni.pe>
2022-06-08 17:01:01 -0500

    update test with save data

--- schainpy/scripts/sophy_proc_rev004.py
+++ schainpy/scripts/sophy_proc_rev004.py
@@ -50,8 +50,8 @@ def main(args):
     N = int(1/(speed_axis[0]*ipp))                                               # 1 GRADO DE RESOLUCION
     path = os.path.join(PATH, experiment, 'rawdata')
     path_ped = os.path.join(PATH, experiment, 'position')
-    path_plots = os.path.join(PATH, experiment, 'plotsC0_PL_R5km')
-    path_save = os.path.join(PATH, experiment, 'param')
+    path_plots = os.path.join(PATH, experiment, 'plotsC0_PL_R'+str(args.range)+'km')
+    path_save = os.path.join(PATH, experiment, 'paramC0_PL_R'+str(args.range)+'km')
     RMIX = 1.62
 
     from schainpy.controller import Project
