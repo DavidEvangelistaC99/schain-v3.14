a8cd1aaa1b7509575d22c6ab87a209f087fd574b
Roberto Flores <rflores@igp.gob.pe>
2023-06-02 16:27:16 -0500

    lagp.f update 7

--- schainf/Ffiles/lagp.f
+++ schainf/Ffiles/lagp.f
@@ -90,6 +90,7 @@ c      write(*,*) "BEFORE GET_PATH"
 c      write(*,*) "L_BEF: ", fqual_temp, "L_BEF_end"
       fqual = TRIM(fqual_temp)
       bfmodel_str = 'bfmodel/'
+      write(*,*) "bfmodel_str: ", bfmodel_str
       fqual(1:len_trim(fqual)-len_trim(bfmodel_str))
       fqual = fqual//'weights.dat'
 c      write(*,*) "Final fqual",fqual,"FINAL"
