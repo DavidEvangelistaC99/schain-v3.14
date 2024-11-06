fe09c4f6aed437fe630f2d66d971286dec0be134
sebastianVP <alex.valdezp22@gmail.com>
2024-11-06 15:43:11 -0500

    Update de parametro bottom en block360 para corregir skipping de graficos en rhi, el bottom reemplaza el 0 en checkcase

--- schainpy/model/proc/jroproc_parameters.py
+++ schainpy/model/proc/jroproc_parameters.py
@@ -4245,7 +4245,7 @@ class Block360(Operation):
     def __init__(self,**kwargs):
         Operation.__init__(self,**kwargs)
 
-    def setup(self, dataOut, attr, angles,horario,heading):
+    def setup(self, dataOut, attr, angles,horario,heading,bottom):
         '''
         n= Numero de PRF's de entrada
         '''
@@ -4262,6 +4262,7 @@ class Block360(Operation):
         self.angles = angles
         self.horario= horario
         self.heading = heading
+        self.bottom  = bottom
 
     def putData(self, data, attr):
         '''
@@ -4384,18 +4385,18 @@ class Block360(Operation):
             middle = self.ele[-2]
             end    = self.ele[-1]
 
-            if end < 0:
+            if end < self.bottom:
                 return 1
             elif (middle>start and end<middle):
                 return -1
 
-    def run(self, dataOut, attr_data='dataPP_POWER', runNextOp = False, angles=[],horario=True,heading=0,**kwargs):
+    def run(self, dataOut, attr_data='dataPP_POWER', runNextOp = False, angles=[],horario=True,heading=0,bottom=0,**kwargs):
 
         dataOut.attr_data = attr_data
         dataOut.runNextOp = runNextOp
 
         if not self.isConfig:
-            self.setup(dataOut=dataOut, attr=attr_data, angles=angles,horario=horario,heading=heading, **kwargs)
+            self.setup(dataOut=dataOut, attr=attr_data, angles=angles,horario=horario, heading=heading,bottom=bottom,**kwargs)
             self.isConfig   = True
 
         data_360, avgdatatime, data_p, data_e, data_n = self.blockOp(dataOut, dataOut.utctime)
