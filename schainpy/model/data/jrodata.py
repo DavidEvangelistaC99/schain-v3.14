b106c4f312a8e2218a605dea5ee0a1b5f35dba31
DavidEvangelistaC99 <david.evangelista.c@gmail.com>
2026-03-25 11:43:46 -0500

    v3.0-devel

--- schainpy/model/data/jrodata.py
+++ schainpy/model/data/jrodata.py
@@ -554,7 +554,7 @@ class Spectra(JROData):
         pwcode = 1
 
         if self.flagDecodeData:
-            pwcode = numpy.sum(self.code[0] ** 2)
+            pwcode = numpy.sum(numpy.abs(self.code[0]) ** 2)
         # normFactor = min(self.nFFTPoints,self.nProfiles)*self.nIncohInt*self.nCohInt*pwcode*self.windowOfFilter
         normFactor = self.nProfiles * self.nIncohInt * self.nCohInt * pwcode * self.windowOfFilter
 
@@ -653,7 +653,7 @@ class SpectraHeis(Spectra):
     def normFactor(self):
         pwcode = 1
         if self.flagDecodeData:
-            pwcode = numpy.sum(self.code[0] ** 2)
+            pwcode = numpy.sum(numpy.abs(self.code[0]) ** 2)
 
         normFactor = self.nIncohInt * self.nCohInt * pwcode
 
