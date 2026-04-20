eb6b07b050f9c13758c06225e04f7b7efc99c763
DavidEvangelistaC99 <david.evangelista.c@gmail.com>
2026-04-20 17:34:55 -0500

    spectra_cut_processing.py script

--- spectra_cut_processing.py
+++ spectra_cut_processing.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 desc = "USRP_test"
 filename = "USRP_processing.xml"
 
-path = '/home/david/Documents/DATA/CHIRP@2025-10-07T19-57-06/rawdata/'
-figpath = '/home/david/Documents/DATA/CHIRP@2025-10-07T19-57-06/rawdata'
+path = '/home/idi/Documents/DATA/CHIRP@2025-08-27T21-34-16/rawdata/'
+figpath = '/home/idi/Documents/DATA/CHIRP@2025-08-27T21-34-16/rawdata'
 
 ## REVISION ##
 ## 1 ##
